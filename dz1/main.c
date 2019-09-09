0bf33e50ff3b83beb7408e8cfd6073560b6e7ea8
Vazhid <vazhid2000@mail.ru>
2019-09-09 13:59:39 +0300

    first update

---
+++ dz1/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  dz1
+//
+//  Created by Vazhid on 02/09/2019.
+//  Copyright © 2019 Vazhid. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    char c=0;
+    char arr[100]={0};
+    int j=0;
+    while((c=getchar()) != EOF) {
+        
+    }
+    printf("%d",arr[j]);
+    return 0;
+}
